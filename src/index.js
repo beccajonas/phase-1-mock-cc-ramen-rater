188438e949a59e51187e18330652234e99c941d0
Becca Jonas <beccajonas3@gmail.com>
2023-11-07 14:52:54 -0500

    solution with advanced deliverables

--- src/index.js
+++ src/index.js
@@ -12,18 +12,22 @@ function fetchData() {
 
 // * Render menu items, add event listener to menu items
 function renderMenu(array) {
+    // Render menu image to menu div
     let menuImage = document.createElement('img')
     menuImage.src = array.image
-
     menuDiv.appendChild(menuImage)
 
-    menuImage.addEventListener('click', () => {
-        let detailDiv = document.querySelector('#ramen-detail')
+    // Select data containers 
+    let detailDiv = document.querySelector('#ramen-detail')
+    let detailImage = document.querySelector('.detail-image')
+    let name = document.querySelector('.name')
+    let restaurant = document.querySelector('.restaurant')
+    let rating = document.querySelector('p')
+    let comment = document.querySelector('#comment-display')
 
-        let detailImage = document.querySelector('.detail-image')
-        let name = document.querySelector('.name')
-        let restaurant = document.querySelector('.restaurant')
-      
+    // On menu image click, render fetched data into containers
+    function clickMenuImage() {
+        menuImage.addEventListener('click', () => {
         detailImage.src = array.image
         detailImage.className = 'detail-image'
 
@@ -33,13 +37,31 @@ function renderMenu(array) {
 
         detailDiv.append(detailImage, name, restaurant)
 
-        let rating = document.querySelector('p')
-        let comment = document.querySelector('#comment-display')
-
         rating.innerText = array.rating
 
         comment.innerText = array.comment
-    })
+    })}
+    clickMenuImage();
+
+    // On menu image dblclick, delete menu image, restore placeholder information in detail div
+    function dblClickMenuImage() {
+        menuImage.addEventListener('dblclick', () => {
+        fetch(`http://localhost:3000/ramens/${array.id}`, {
+            method: 'DELETE',
+        })
+        .then(response => {
+            console.log(response, 'Deleting');
+            if (response.status === 200) {
+                menuImage.remove()
+                detailImage.src = './assets/image-placeholder.jpg'
+                name.innerText = 'Insert Name Here'
+                restaurant.innerText = 'Insert Restaurant Here'
+                rating.innerText = ' / 10'
+                comment.innerText = 'Insert comment here'
+            }
+        })
+    })}
+    dblClickMenuImage(); 
 }
 
 // * Submit new form 
@@ -49,20 +71,36 @@ function formSubmission() {
     form.addEventListener('submit', handleSubmit)
 
     function handleSubmit(e) {
-        
-        e.preventDefault()
+        e.preventDefault();
         
         let image = document.createElement('img')
         image.src = e.target[2].value
 
-        // let name = e.target[0].value
-        // let restaurant = e.target[1].value
-        // let rating = e.target[3].value
-        // let comment = e.target[4].value
-
         menuDiv.append(image)
+        
+        let name = e.target[0].value
+        let restaurant = e.target[1].value
+        let rating = e.target[3].value
+        let comment = e.target[4].value
+
+        form.reset()
+
+        fetch('http://localhost:3000/ramens', {
+            method: 'POST',
+            headers: {'content-type' : 'application/JSON'},
+            body: JSON.stringify({
+                name: name ,
+                restaurant: restaurant,
+                image: image.src,
+                rating: rating,
+                comment: comment})
+        })
+        .then(response => response.json())
+        .then(data => {console.log(data);
+        }) 
     } 
 }
+
 
 
 
